e89a95371a5e2c89b7692c5c6af7ac34ef7fca69
Macielbessa <isaacbessa02@gmail.com>
2022-06-22 21:27:10 -0300

    projeto

--- CaixaEletronico.java
+++ CaixaEletronico.java
@@ -4,14 +4,13 @@ import java.math.BigDecimal;
 
 public class CaixaEletronico {
     public void sacarSaldo(ContaCorrente corrente, BigDecimal valorParaSacar) throws ExceptionDoSaldo {
-
-            if (valorParaSacar.compareTo(corrente.getSaldo()) > 0) {
-                BigDecimal valorAtual = corrente.getSaldo().subtract(valorParaSacar);
-                corrente.setSaldo(valorAtual);
-                System.out.println("saque pare retirar: R$" + valorParaSacar);
-                System.out.println("saldo atualizado de: R$ " + corrente.getSaldo());
-            } else {
-                throw new ExceptionDoSaldo("Sem saldo");
-            }
-       }
+        if (valorParaSacar.compareTo(corrente.getSaldo()) > 0) {
+            BigDecimal valorAtual = corrente.getSaldo().subtract(valorParaSacar);
+            corrente.setSaldo(valorAtual);
+            System.out.println("saque pare retirar: R$" + valorParaSacar);
+            System.out.println("saldo atualizado de: R$ " + corrente.getSaldo());
+        } else {
+            throw new ExceptionDoSaldo("Sem saldo");
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -4,14 +4,18 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) throws ExceptionDoSaldo {
-        ContaCorrente contaCorrenteDoJõao = new ContaCorrente();
+        try {
+            ContaCorrente contaCorrenteDoJõao = new ContaCorrente();
 
-        contaCorrenteDoJõao.setCliente("João"); // cliente
-        contaCorrenteDoJõao.setSaldo(new BigDecimal (100.00)); // saldo atual
+            contaCorrenteDoJõao.setCliente("João"); // cliente
+            contaCorrenteDoJõao.setSaldo(new BigDecimal (100.00)); // saldo atual
 
-        CaixaEletronico caixaEletronicoDoJoão = new CaixaEletronico();
-        caixaEletronicoDoJoão.sacarSaldo(contaCorrenteDoJõao,new BigDecimal(50.00)); // valor a ser sacado
+            CaixaEletronico caixaEletronicoDoJoão = new CaixaEletronico();
+            caixaEletronicoDoJoão.sacarSaldo(contaCorrenteDoJõao,new BigDecimal(50.00)); // valor a ser sacado
 
-        contaCorrenteDoJõao.logarSaldoAtual();
+            contaCorrenteDoJõao.logarSaldoAtual();
+        } catch (ExceptionDoSaldo e){
+            System.out.println(e.getMessage());
         }
+    }
 }
