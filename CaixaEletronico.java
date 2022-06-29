4fa13a9ebc0fc9e54758209c63c7d0ea67a25d4c
Macielbessa <isaacbessa02@gmail.com>
2022-06-29 17:33:42 -0300

    deposito

--- CaixaEletronico.java
+++ CaixaEletronico.java
@@ -4,21 +4,19 @@ import java.math.BigDecimal;
 
 public class CaixaEletronico {
     public void sacarSaldo(ContaCorrente corrente, BigDecimal valorParaSacar) throws ExceptionDoSaldo {
-        if (valorParaSacar.compareTo(corrente.getSaldo()) < 10) {
+        if (valorParaSacar.compareTo(corrente.getSaldo()) < 0) {
             BigDecimal valorAtual = corrente.getSaldo().subtract(valorParaSacar);
             corrente.setSaldo(valorAtual);
-            System.out.println("saque pare retirar: R$" + valorParaSacar);
-            System.out.println("saldo atualizado de: R$ " + corrente.getSaldo());
+            System.out.println("quero sacar : R$" + valorParaSacar);
+            System.out.println("saldo atualizado de : " + corrente.getSaldo());
         } else {
             throw new ExceptionDoSaldo("Sem saldo");
         }
     }
 
     public void depositar(ContaCorrente conta, BigDecimal valorParaDepositar) throws ExceptionDoDeposito {
-        if (valorParaDepositar.compareTo(valorParaDepositar) > 0) {
-            BigDecimal valorParaDeposito = conta.getSaldo().add(valorParaDepositar);
-            conta.setSaldo(valorParaDeposito);
-            System.out.println("valor para depositar R$" + valorParaDepositar);
+        if (valorParaDepositar.compareTo(valorParaDepositar) < 1) {
+            System.out.println("valor para depositar: R$" + valorParaDepositar);
         } else {
             throw new ExceptionDoDeposito("Deposito negado");
         }
--- Main.java
+++ Main.java
@@ -10,16 +10,15 @@ public class Main {
 
             contaCorrenteDoJõao.setCliente("João"); // nome do cliente
 
-            contaCorrenteDoJõao.setSaldo(new BigDecimal (50.00)); // saldo atual do cliente
+            contaCorrenteDoJõao.setSaldo(new BigDecimal (500.00)); // saldo atual do cliente
 
             CaixaEletronico caixaEletronicoDoJoão = new CaixaEletronico();
 
-            caixaEletronicoDoJoão.depositar(contaCorrenteDoJõao, new BigDecimal(200.00 ).add(contaCorrenteDoJõao.getSaldo())); // depositando na conta
+            caixaEletronicoDoJoão.depositar(contaCorrenteDoJõao, new BigDecimal(500.00 )); // depositando na conta
 
-            caixaEletronicoDoJoão.sacarSaldo(contaCorrenteDoJõao,new BigDecimal(50.00)); // valor a ser sacado da conta do cliente
+            caixaEletronicoDoJoão.sacarSaldo(contaCorrenteDoJõao,new BigDecimal(500.00).divide(new BigDecimal(2.0))); // valor a ser sacado da conta do cliente
 
             contaCorrenteDoJõao.logarSaldoAtual();
-
 
         } catch (ExceptionDoSaldo e){
             System.out.println(e.getMessage());
